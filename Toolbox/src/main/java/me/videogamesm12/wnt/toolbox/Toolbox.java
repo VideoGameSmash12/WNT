7d2ad766444e0d56e39f834e7b85a327c218dee0
Video <videogamesm12@gmail.com>
2022-05-26 19:11:14 -0600

    Removes the Telnet client from the Toolbox

--- Toolbox/src/main/java/me/videogamesm12/wnt/toolbox/Toolbox.java
+++ Toolbox/src/main/java/me/videogamesm12/wnt/toolbox/Toolbox.java
@@ -22,12 +22,9 @@
 
 package me.videogamesm12.wnt.toolbox;
 
-import me.videogamesm12.wnt.WNT;
 import me.videogamesm12.wnt.command.CommandSystem;
 import me.videogamesm12.wnt.toolbox.commands.NameCommand;
-import me.videogamesm12.wnt.toolbox.commands.TelnetCommand;
 import me.videogamesm12.wnt.toolbox.commands.UuidCommand;
-import me.videogamesm12.wnt.toolbox.modules.NuTelnet;
 import net.fabricmc.api.ModInitializer;
 
 public class Toolbox implements ModInitializer
@@ -35,10 +32,7 @@ public class Toolbox implements ModInitializer
     @Override
     public void onInitialize()
     {
-        WNT.MODULES.register(NuTelnet.class);
-        //--
         CommandSystem.registerCommand(NameCommand.class);
         CommandSystem.registerCommand(UuidCommand.class);
-        CommandSystem.registerCommand(TelnetCommand.class);
     }
 }
