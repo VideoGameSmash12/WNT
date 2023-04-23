30825392c3d8dd42892c073511576df917e7ee96
Allink <44676012+allinkdev@users.noreply.github.com>
2023-04-23 05:28:58 +0100

    Do not allow Minecraft to set java.awt.headless to true (#20)
    For whatever reason, Mojang thought it was a spectacular idea to set java.awt.headless to false in the clinit of net.minecraft.client.main.Main.class.
    Perhaps it increases performance? But that doesn't particularly matter in our case, as we need to use AWT for the Blackbox module.
    I also removed the headless set that Blackbox automatically does when you try to open it, as this fix makes it redundant.
    Thanks to @LunaWasFlaggedAgain for pointing this out.
    Fixes #18

---
+++ Blackbox/src/main/java/me/videogamesm12/wnt/blackbox/mixin/MainMixin.java
@@ -0,0 +1,16 @@
+package me.videogamesm12.wnt.blackbox.mixin;
+
+import net.minecraft.client.main.Main;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Main.class)
+public class MainMixin
+{
+    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Ljava/lang/System;setProperty(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;"))
+    private static String setProperty(String key, String value)
+    {
+        return System.setProperty("java.awt.headless", "false");
+    }
+}
